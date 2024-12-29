5d195524c11439dd07909717c38de468653e45e5
GIGABYTE <rimu.appdeveloper@gmail.com>
2024-12-29 11:29:32 +0600

    module15

---
+++ bin/module_fifteen/test_two.dart
@@ -0,0 +1,28 @@
+
+import 'dart:io';
+
+String transformString(String input) {
+  String result = "";
+
+  for (int i = 0; i < input.length; i++) {
+    result += input[i];
+
+
+    if ((i + 1) % 2 == 0 && i != input.length - 1) {
+      result += 'x';
+    }
+  }
+
+  return result;
+}
+
+void main() {
+  // Write your dart code from here
+  String? userInput = stdin.readLineSync();
+  if (userInput != null && userInput.isNotEmpty) {
+    String output = transformString(userInput);
+    print(output);
+  } else {
+    print("Please enter a valid string.");
+  }
+}
